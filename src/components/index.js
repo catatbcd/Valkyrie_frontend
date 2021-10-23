c6b47fe88c6e7039e0725279a54abe7b17bcf4c0
Catalina Torrente <caatoba@gmail.com>
2021-10-23 04:34:27 -0500

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Navigation } from "./Navigation";
-export { default as Home } from "./Home";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 export { default as Products } from "./Products";
